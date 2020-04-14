8c755bfba8618565d877b8a64bc94dfb2a749c5e
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-14 14:00:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,7 +22,7 @@ def input_to_index(user_input)
 end
 
 def move(board, index, current_player = "X")
-  board[index] = current_player
+  board[index.to_i-1] = current_player
 end
 
 def position_taken?(board, location)
