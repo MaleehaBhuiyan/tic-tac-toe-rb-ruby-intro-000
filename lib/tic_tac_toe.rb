b51eac43e0425d3b8b4ae19a8dc2b00c1611922e
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-14 14:23:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,3 +48,13 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board_array)
+  counter = 0
+  board_array.each do |index|
+    if index == "X" || index == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
