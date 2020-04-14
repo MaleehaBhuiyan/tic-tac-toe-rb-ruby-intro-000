ea99ae7f5c94d72763d93054b205ac72a9662aa7
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-14 14:12:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,3 +29,10 @@ end
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+
+def valid_move?(board,index)
+  index.between?(0,8)
+  if !position_taken?(board,index) && index.between?(0,8)
+    true
+  end
+end
